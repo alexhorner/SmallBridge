94b3bdf24887da0a5d46737e5580ad5308a0a79a
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2020-07-11 15:53:23 +0100

    Example config and mapping

---
+++ SmallBridge/Config/ChannelMappingConfig.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SmallBridge.Config
+{
+    public class ChannelMappingConfig
+    {
+        [JsonProperty("irc_channel")] public string IrcChannel { get; set; }
+        [JsonProperty("discord_channel")] public ulong DiscordChannel { get; set; }
+    }
+}
---
+++ SmallBridge/Config/MainConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SmallBridge.Config
+{
+    public class MainConfig
+    {
+        [JsonProperty("discord_token")] public string DiscordToken { get; set; }
+        [JsonProperty("discord_server")] public ulong DiscordServer { get; set; }
+        [JsonProperty("private_messages_discord_channel")] public ulong DiscordPmChannel { get; set; }
+        [JsonProperty("private_messages_irc_channel")] public string IrcPmChannel { get; set; }
+        [JsonProperty("channel_mapping")] public List<ChannelMappingConfig> ChannelMappings { get; set; }
+        
+        public static MainConfig LoadFromJsonFile(string path)
+        {
+            return JsonConvert.DeserializeObject<MainConfig>(File.ReadAllText(path));
+        }
+    }
+}
